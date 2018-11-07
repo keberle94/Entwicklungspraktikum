d2f6dd6e7379919ae8baeb7dbbdebf9b2e07f839
Kevin Eberle <niveke@web.de>
2018-11-07 11:16:45 +0100

    Add JavaScript file and change colors on check
    - New Java Script file is created and linked in the html
    - Html now uses js methods to change backgroundcolor of the cell
      dependent on value

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//changeColorToTrue
+
+function chColorToTrue(elementID) {
+    document.getElementById(elementID).style.backgroundColor = "#01ff80";
+}
+
+//changeColorToFalse
+
+function chColorToFalse(elementID) {
+    document.getElementById(elementID).style.backgroundColor = "#ff4837";
+}
+
+//testForValueInTolerance
+
+function testForValueInTolerance(givenValue, maxValue, minValue, elementID) {
+    if (givenValue < minValue || givenValue > maxValue)
+        chColorToFalse(elementID);
+    else
+        chColorToTrue(elementID);
+}
